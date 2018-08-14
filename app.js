b0ccb52d10d6604a3b3fc7ecca559c6faf610591
Oskar Rough <oskar@rough.dk>
2018-08-14 22:58:50 +0200

    WIP

---
+++ app.js
@@ -0,0 +1,43 @@
+// import data from './data.js'
+console.log({data})
+
+const {hyper, Component} = hyperHTML
+// const {bind: hyper, wire} = hyperHTML
+
+class VideoGrid extends Component {
+  get defaultState() {
+    return {tracks: data.tracks}
+  }
+  render() {
+    var fallback = 'https://media.giphy.com/media/X6kSlteTQtIdWeYO6Y/giphy.gif'
+    return this.html`
+			${this.state.tracks.map(
+        (item, index) => `
+				<article>
+					<video-placeholder>
+						<button is="video-placeholder-front">
+							<img src="${item.gif ? item.gif : fallback}" alt="${item.title}">
+							<h2>${item.title}</h2>
+							<svg class="PlayButton" viewBox="0 0 200 200" alt="Play video">
+								<circle cx="100" cy="100" r="90" fill="none" stroke-width="15" stroke="#fff" />
+								<polygon points="70, 55 70, 145 145, 100" fill="#fff" />
+							</svg>
+						</button>
+						<template>
+							<iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/${
+                item.youtubeId
+              }?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+						</template>
+					</video-placeholder>
+				</article>
+			`
+      )}
+		`
+  }
+}
+
+function init() {
+  hyper(document.querySelector('.VideoGrid'))`${new VideoGrid()}`
+}
+
+document.addEventListener('DOMContentLoaded', init)
---
+++ data.js
@@ -0,0 +1,109 @@
+const data = {}
+
+data.tracks = [
+  {
+    trackNumber: 'A1',
+    title: 'Entrance',
+    youtubeId: 'RaWxiCl8Dn8',
+    gif: 'https://media.giphy.com/media/X6kSlteTQtIdWeYO6Y/giphy.gif'
+  },
+  {
+    trackNumber: 'A2',
+    title: 'Level Zero',
+    youtubeId: 'RaWxiCl8Dn8',
+    gif: 'https://media.giphy.com/media/Zy7HTSu2oD2f6VZEfl/giphy.gif'
+  },
+  {
+    trackNumber: 'A3',
+    title: 'No Hay Trabajo Para Mi',
+		youtubeId: 'fKsNu1AAJ6M',
+		gif: 'https://media.giphy.com/media/FDcvH6Icd9WAQRxHHx/giphy.gif'
+  },
+  {
+    trackNumber: 'A4',
+    title: 'Albania FM feat. Funkycan',
+		youtubeId: 'WabxtaB0yzM',
+		gif: 'https://media.giphy.com/media/3gNowi8xp8PoWwdb0T/giphy.gif'
+  },
+  {
+    trackNumber: 'B1',
+    title: 'Intershop',
+		youtubeId: 'j7hc4lCJkjQ',
+		gif: 'https://media.giphy.com/media/24FIhzP88RWJoO7Igt/giphy.gif'
+  },
+  {
+    trackNumber: 'B2',
+    title: 'Midi Break 1',
+    youtubeId: 'RaWxiCl8Dn8'
+  },
+  {
+    trackNumber: 'B3',
+    title: 'Srrr',
+    youtubeId: 'RaWxiCl8Dn8'
+  },
+  {
+    trackNumber: 'B4',
+    title: 'Midi Lisa',
+    youtubeId: 'RaWxiCl8Dn8'
+  },
+  {
+    trackNumber: 'B5',
+    title: 'Funk',
+    youtubeId: 'RaWxiCl8Dn8'
+  },
+  {
+    trackNumber: 'B6',
+    title: 'Master Quest',
+    youtubeId: 'RaWxiCl8Dn8'
+  },
+  {
+    trackNumber: 'C1',
+    title: 'Y',
+    youtubeId: 'RaWxiCl8Dn8'
+  },
+  {
+    trackNumber: 'C2',
+    title: 'Arcadia',
+    youtubeId: 'RaWxiCl8Dn8'
+  },
+  {
+    trackNumber: 'C3',
+    title: 'Termina feat. Gerry Franke',
+    youtubeId: 'RaWxiCl8Dn8'
+  },
+  {
+    trackNumber: 'C4',
+    title: 'Rush',
+    youtubeId: 'RaWxiCl8Dn8'
+  },
+  {
+    trackNumber: 'C5',
+    title: 'Lozt feat. Ludi',
+    youtubeId: 'RaWxiCl8Dn8'
+  },
+  {
+    trackNumber: 'C6',
+    title: 'Midi Break 2',
+    youtubeId: 'RaWxiCl8Dn8'
+  },
+  {
+    trackNumber: 'D1',
+    title: 'Shrimp',
+    youtubeId: 'RaWxiCl8Dn8'
+  },
+  {
+    trackNumber: 'D2',
+    title: 'All my Circuits',
+    youtubeId: 'RaWxiCl8Dn8'
+  },
+  {
+    trackNumber: 'D3',
+    title: 'Burning 4 U feat. Chrissley Benz',
+    youtubeId: 'RaWxiCl8Dn8'
+  },
+  {
+    trackNumber: 'D4',
+    title: 'Exit',
+    youtubeId: 'RaWxiCl8Dn8'
+  }
+]
