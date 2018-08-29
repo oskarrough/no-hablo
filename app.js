aaac19736a7b6e9a23510cc1d95031ded8ffbdce
Oskar Rough <oskar@rough.dk>
2018-08-29 16:45:19 +0200

    Nicer overlays and grid layout

--- app.js
+++ app.js
@@ -1,32 +1,48 @@
 // import data from './data.js'
-console.log({data})
+// console.log({data})
 
 const {hyper, Component} = hyperHTML
 // const {bind: hyper, wire} = hyperHTML
 
 class VideoGrid extends Component {
   get defaultState() {
-    return {tracks: data.tracks}
+    return {
+      tracks: data.tracks,
+      releases: data.releases
+    }
   }
   render() {
     var fallback = 'https://media.giphy.com/media/X6kSlteTQtIdWeYO6Y/giphy.gif'
+
     return this.html`
-			${this.state.tracks.map(
-        (item, index) => `
-				<a href="https://www.youtube.com/watch?v=${
-          item.youtubeId
-        }&amp;autoplay=1&amp;rel=0&amp;controls=1&amp;showinfo=0" data-fancybox="gallery" data-caption="${
-          item.trackNumber
-        } ${item.title}">
-					<img src="${item.gif ? item.gif : fallback}" alt="${item.title}">
-					<h2 class="font-normal">
-						<span class="opacity-50">${item.trackNumber}</span> ${item.title}
+			${this.state.releases.map(
+        (release, index) => `
+				<div class="Release">
+					<h2 class="">
+						${release[0].trackNumber.charAt(0)}
 					</h2>
-					<svg class="PlayButton" viewBox="0 0 200 200" alt="Play video">
-						<circle cx="100" cy="100" r="90" fill="none" stroke-width="15" stroke="#fff" />
-						<polygon points="70, 55 70, 145 145, 100" fill="#fff" />
-					</svg>
-				</a>
+<div class="flex flex-wrap">
+				${release
+          .map(
+            track => `
+					<a class="Track" href="https://www.youtube.com/watch?v=${
+            track.youtubeId
+          }&amp;autoplay=1&amp;rel=0&amp;controls=1&amp;showinfo=0" data-fancybox="gallery" data-caption="${
+              track.trackNumber
+            } ${track.title}">
+						<img src="${track.gif ? track.gif : fallback}" alt="${track.title}">
+						<h2 class="font-normal">
+							<span class="opacity-50">${track.trackNumber}</span> ${track.title}
+						</h2>
+						<svg class="PlayButton" viewBox="0 0 200 200" alt="Play video">
+							<circle cx="100" cy="100" r="90" fill="none" stroke-width="15" stroke="#fff" />
+							<polygon points="70, 55 70, 145 145, 100" fill="#fff" />
+						</svg>
+					</a>
+				`
+          )
+          .join('')}</div>
+				</div>
 			`
       )}
 		`
@@ -35,6 +51,12 @@ class VideoGrid extends Component {
 
 function init() {
   hyper(document.querySelector('.VideoGrid'))`${new VideoGrid()}`
+
+  $('.Order-gallery > a').fancybox({
+    type: 'image',
+    hash: false,
+    buttons: ['zoom', 'close']
+  })
 
   $('.VideoGrid [data-fancybox]').fancybox({
     animationDuration: 200,
@@ -57,12 +79,23 @@ function init() {
 
   var $overlay = $('.Order')
   var $overlayButtons = $('.js-toggleOrder')
-  $overlayButtons.on('click', () => {
+
+  function toggleOverlay() {
     $overlay.toggleClass('is-open')
-  })
-  $('body').keydown((event) => {
+    $('body').toggleClass('is-fadedOut')
+  }
+
+  function closeOverlay() {
+    $overlay.removeClass('is-open')
+    $('body').removeClass('is-fadedOut')
+  }
+
+  $overlayButtons.on('click', toggleOverlay)
+  $('.Overlay').on('click', closeOverlay)
+
+  $('body').keydown(event => {
     if (event.key === 'Escape' && $overlay.hasClass('is-open')) {
-      $overlay.removeClass('is-open')
+      closeOverlay()
     }
   })
 }
