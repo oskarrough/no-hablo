1e805bb973299f70c51384f2b5d72c21c6581f86
Oskar Rough <oskar@rough.dk>
2018-09-05 08:44:09 +0200

    Rewrite javascript templates without classes

--- app.js
+++ app.js
@@ -1,73 +1,64 @@
 /* global $, data */
 
-const {hyper, Component} = hyperHTML
+const {hyper, wire} = hyperHTML
 
-class VideoGrid extends Component {
-	get defaultState() {
-		return {
-			releases: data.releases
-		}
-	}
-	render() {
-		return this.html`
-			${this.state.releases.map(
-				(release, index) => `
-				<div class="Container">
-					<div class="flex flex-wrap">
-				${release
-					.map(
-						track => `
-					<a class="Track" href="https://www.youtube.com/watch?v=${
-						track.youtubeId
-					}&amp;autoplay=1&amp;rel=0&amp;controls=1&amp;showinfo=0" data-fancybox="gallery" data-caption="${
-							track.trackNumber
-						} ${track.title}">
+function List(items) {
+  return wire(items)`
+		<div class="Container">
+			${items.map(ReleaseTemplate)}
+		</div>`
+}
 
-						<!-- <img class="lazyload" data-src="${track.gif}" alt=""> -->
-						<video muted autoplay loop class="lazyload" src="${track.video}"></video>
+function ReleaseTemplate(release) {
+  return wire(release)`
+		<div class="flex flex-wrap">
+			${release.map(TrackTemplate)}
+		</div>`
+}
 
-						<h2 class="font-normal">
-							<span class="opacity-50">${track.trackNumber}</span> ${track.title}
-						</h2>
-						<svg class="PlayButton" viewBox="0 0 200 200" alt="Play video">
-							<circle cx="100" cy="100" r="90" fill="none" stroke-width="15" stroke="#fff" />
-							<polygon points="70, 55 70, 145 145, 100" fill="#fff" />
-						</svg>
-					</a>
-				`
-					)
-					.join('')}</div>
-				</div>
-			`
-			)}
-		`
-	}
+function TrackTemplate(track) {
+  const href = `https://www.youtube.com/watch?v=${
+    track.youtubeId
+  }&amp;autoplay=1&amp;rel=0&amp;controls=1&amp;showinfo=0"`
+  return wire(track)`
+		<a class="Track" href="${href}" data-fancybox="gallery">
+			<video muted autoplay loop class="lazyload" src="${track.video}"></video>
+			<h2>
+				<span class="opacity-50">${track.trackNumber}</span> ${track.title}
+			</h2>
+			<svg class="PlayButton" viewBox="0 0 200 200" alt="Play video">
+				<circle cx="100" cy="100" r="90" fill="none" stroke-width="15" stroke="antiquewhite" />
+				<polygon points="70, 55 70, 145 145, 100" fill="antiquewhite" />
+			</svg>
+		</a>`
 }
 
 function init() {
-	hyper(document.querySelector('.VideoGrid'))`${new VideoGrid()}`
+  // Template with video grid. Also see data.js file.
+  hyper(document.querySelector('.VideoGrid'))`${new List(data.releases)}`
 
-	$('.VideoGrid [data-fancybox]').fancybox({
-		animationDuration: 200,
-		hash: false,
-		buttons: [
-			'close'
-		],
-		infobar: false,
-		caption: function(instance, item) {
-			var caption = this.querySelector('h2').outerHTML
-			var count = '<span data-fancybox-index></span> of <span data-fancybox-count></span>'
-			return count + caption
-		}
-	})
-
-	$('.Order-gallery > a').fancybox({
+  // Video gallery.
+  $('.VideoGrid [data-fancybox]').fancybox({
     animationDuration: 200,
-		type: 'image',
-		hash: false,
-		buttons: ['zoom', 'close']
-	})
+    hash: false,
+    buttons: ['close'],
+    infobar: false,
+    caption: function(instance, item) {
+      var caption = this.querySelector('h2').outerHTML
+      var count = '<span data-fancybox-index></span> of <span data-fancybox-count></span>'
+      return count + caption
+    }
+  })
 
+  // Image gallery.
+  $('.Order-gallery > a').fancybox({
+    animationDuration: 200,
+    type: 'image',
+    hash: false,
+    buttons: ['zoom', 'close']
+  })
+
+  // Smooth scrolling.
   $('.js-scroll').on('click', function(event) {
     event.preventDefault()
     document.querySelector($(this).attr('href')).scrollIntoView({
