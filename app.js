e725a605e32481833d650f002b3ed8f11d13b2f2
Oskar Rough <oskar@rough.dk>
2018-09-05 22:10:54 +0200

    Add new videos

--- app.js
+++ app.js
@@ -3,26 +3,42 @@
 const {hyper, wire} = hyperHTML
 
 function List(items) {
-  return wire(items)`
+	return wire(items)`
 		<div class="Container">
 			${items.map(ReleaseTemplate)}
 		</div>`
 }
 
 function ReleaseTemplate(release) {
-  return wire(release)`
+	return wire(release)`
 		<div class="flex flex-wrap">
 			${release.map(TrackTemplate)}
 		</div>`
 }
 
 function TrackTemplate(track) {
-  const href = `https://www.youtube.com/watch?v=${
-    track.youtubeId
-  }&amp;autoplay=1&amp;rel=0&amp;controls=1&amp;showinfo=0"`
-  return wire(track)`
+	const href = `https://www.youtube.com/watch?v=${
+		track.youtubeId
+	}&autoplay=1&rel=0&controls=1&showinfo=0&list=PLGXSVM0qM_LqHgm0N99p4g9z1G6GCEu2z"`
+
+	const thumb = {
+		hq: `http://img.youtube.com/vi/${track.youtubeId}/hqdefault.jpg`,
+		maxres: `https://img.youtube.com/vi/${track.youtubeId}/maxresdefault.jpg`,
+		webp: `https://img.youtube.com/vi_webp/${track.youtubeId}/maxresdefault.webp`
+	}
+
+	const giphy = {
+		page: `https://giphy.com/gifs/${track.giphyId}/media`,
+		small: `https://media.giphy.com/media/${track.giphyId}/200w_d.gif`,
+		social: `https://media.giphy.com/media/${track.giphyId}/giphy.gif`,
+		video: `https://media.giphy.com/media/${track.giphyId}/giphy.mp4`
+	}
+
+	return wire(track)`
 		<a class="Track" href="${href}" data-fancybox="gallery">
-			<video muted autoplay loop class="lazyload" src="${track.video}"></video>
+			<video muted autoplay loop class="lazyload" src="${giphy.video}" placeholder="${
+		thumb.maxres
+	}"></video>
 			<h2>
 				<span class="opacity-50">${track.trackNumber}</span> ${track.title}
 			</h2>
@@ -34,37 +50,37 @@ function TrackTemplate(track) {
 }
 
 function init() {
-  // Template with video grid. Also see data.js file.
-  hyper(document.querySelector('.VideoGrid'))`${new List(data.releases)}`
+	// Template with video grid. Also see data.js file.
+	hyper(document.querySelector('.VideoGrid'))`${new List(data.releases)}`
 
-  // Video gallery.
-  $('.VideoGrid [data-fancybox]').fancybox({
-    animationDuration: 200,
-    hash: false,
-    buttons: ['close'],
-    infobar: false,
-    caption: function(instance, item) {
-      var caption = this.querySelector('h2').outerHTML
-      var count = '<span data-fancybox-index></span> of <span data-fancybox-count></span>'
-      return count + caption
-    }
-  })
+	// Video gallery.
+	$('.VideoGrid [data-fancybox]').fancybox({
+		animationDuration: 200,
+		hash: false,
+		buttons: ['close'],
+		infobar: false,
+		caption: function(instance, item) {
+			var caption = this.querySelector('h2').outerHTML
+			var count = '<span data-fancybox-index></span> of <span data-fancybox-count></span>'
+			return count + caption
+		}
+	})
 
-  // Image gallery.
-  $('.Order-gallery > a').fancybox({
-    animationDuration: 200,
-    type: 'image',
-    hash: false,
-    buttons: ['zoom', 'close']
-  })
+	// Image gallery.
+	$('.Order-gallery > a').fancybox({
+		animationDuration: 200,
+		type: 'image',
+		hash: false,
+		buttons: ['zoom', 'close']
+	})
 
-  // Smooth scrolling.
-  $('.js-scroll').on('click', function(event) {
-    event.preventDefault()
-    document.querySelector($(this).attr('href')).scrollIntoView({
-      behavior: 'smooth'
-    })
-  })
+	// Smooth scrolling.
+	$('.js-scroll').on('click', function(event) {
+		event.preventDefault()
+		document.querySelector($(this).attr('href')).scrollIntoView({
+			behavior: 'smooth'
+		})
+	})
 }
 
 document.addEventListener('DOMContentLoaded', init)
--- data.js
+++ data.js
@@ -1,144 +1,140 @@
 const data = {}
 
+// https://www.youtube.com/playlist?list=PLGXSVM0qM_LqHgm0N99p4g9z1G6GCEu2z
+// missing: A2, B3, B5, C2
+
 data.releases = [
-  [{
-    trackNumber: 'A1',
-    title: 'Entrance',
-    youtubeId: 'RaWxiCl8Dn8',
-    gif: 'https://media.giphy.com/media/X6kSlteTQtIdWeYO6Y/giphy.gif',
-    video: 'https://media.giphy.com/media/X6kSlteTQtIdWeYO6Y/giphy.mp4'
-  },
-  {
-    trackNumber: 'A2',
-    title: 'Level Zero',
-    youtubeId: 'RaWxiCl8Dn8',
-    gif: 'https://media.giphy.com/media/Zy7HTSu2oD2f6VZEfl/giphy-downsized-large.gif',
-    video: 'https://media.giphy.com/media/Zy7HTSu2oD2f6VZEfl/giphy.mp4'
-  },
-  {
-    trackNumber: 'A3',
-    title: 'No Hay Trabajo Para Mi',
-    youtubeId: 'fKsNu1AAJ6M',
-    gif: 'https://media.giphy.com/media/FDcvH6Icd9WAQRxHHx/giphy.gif',
-    video: 'https://media.giphy.com/media/FDcvH6Icd9WAQRxHHx/giphy.mp4'
-  },
-  {
-    trackNumber: 'A4',
-    title: 'Albania FM feat. Funkycan',
-    youtubeId: 'WabxtaB0yzM',
-    gif: 'https://media.giphy.com/media/3gNowi8xp8PoWwdb0T/giphy.gif',
-    video: 'https://media.giphy.com/media/3gNowi8xp8PoWwdb0T/giphy.mp4'
-  }],
-  [{
-    trackNumber: 'B1',
-    title: 'Intershop',
-    youtubeId: 'j7hc4lCJkjQ',
-    gif: 'https://media.giphy.com/media/24FIhzP88RWJoO7Igt/giphy.gif',
-    video: 'https://media.giphy.com/media/24FIhzP88RWJoO7Igt/giphy.mp4'
-  },
-  {
-    trackNumber: 'B2',
-    title: 'Midi Break 1',
-    youtubeId: 'RaWxiCl8Dn8',
-    gif: 'https://media.giphy.com/media/Zy7HTSu2oD2f6VZEfl/giphy-downsized-large.gif',
-    video: 'https://media.giphy.com/media/Zy7HTSu2oD2f6VZEfl/giphy.mp4'
-  },
-  {
-    trackNumber: 'B3',
-    title: 'Srrr',
-    youtubeId: 'RaWxiCl8Dn8',
-    gif: 'https://media.giphy.com/media/FDcvH6Icd9WAQRxHHx/giphy.gif',
-    video: 'https://media.giphy.com/media/FDcvH6Icd9WAQRxHHx/giphy.mp4'
-  },
-  {
-    trackNumber: 'B4',
-    title: 'Midi Lisa',
-    youtubeId: 'RaWxiCl8Dn8',
-    gif: 'https://media.giphy.com/media/3gNowi8xp8PoWwdb0T/giphy.gif',
-    video: 'https://media.giphy.com/media/3gNowi8xp8PoWwdb0T/giphy.mp4'
-  },
-  {
-    trackNumber: 'B5',
-    title: 'Funk',
-    youtubeId: 'RaWxiCl8Dn8',
-    gif: 'https://media.giphy.com/media/24FIhzP88RWJoO7Igt/giphy.gif',
-    video: 'https://media.giphy.com/media/24FIhzP88RWJoO7Igt/giphy.mp4'
-  },
-  {
-    trackNumber: 'B6',
-    title: 'Master Quest',
-    youtubeId: 'RaWxiCl8Dn8',
-    gif: 'https://media.giphy.com/media/FDcvH6Icd9WAQRxHHx/giphy.gif',
-    video: 'https://media.giphy.com/media/FDcvH6Icd9WAQRxHHx/giphy.mp4'
-  }],
-  [{
-    trackNumber: 'C1',
-    title: 'Y',
-    youtubeId: 'RaWxiCl8Dn8',
-    gif: 'https://media.giphy.com/media/Zy7HTSu2oD2f6VZEfl/giphy-downsized-large.gif',
-    video: 'https://media.giphy.com/media/Zy7HTSu2oD2f6VZEfl/giphy.mp4'
-  },
-  {
-    trackNumber: 'C2',
-    title: 'Arcadia',
-    youtubeId: 'RaWxiCl8Dn8',
-    gif: 'https://media.giphy.com/media/24FIhzP88RWJoO7Igt/giphy.gif',
-    video: 'https://media.giphy.com/media/24FIhzP88RWJoO7Igt/giphy.mp4'
-  },
-  {
-    trackNumber: 'C3',
-    title: 'Termina feat. Gerry Franke',
-    youtubeId: 'RaWxiCl8Dn8',
-    gif: 'https://media.giphy.com/media/Zy7HTSu2oD2f6VZEfl/giphy-downsized-large.gif',
-    video: 'https://media.giphy.com/media/Zy7HTSu2oD2f6VZEfl/giphy.mp4'
-  },
-  {
-    trackNumber: 'C4',
-    title: 'Rush',
-    youtubeId: 'RaWxiCl8Dn8',
-    gif: 'https://media.giphy.com/media/Zy7HTSu2oD2f6VZEfl/giphy-downsized-large.gif',
-    video: 'https://media.giphy.com/media/Zy7HTSu2oD2f6VZEfl/giphy.mp4'
-  },
-  {
-    trackNumber: 'C5',
-    title: 'Lozt feat. Ludi',
-    youtubeId: 'RaWxiCl8Dn8',
-    gif: 'https://media.giphy.com/media/3gNowi8xp8PoWwdb0T/giphy.gif',
-    video: 'https://media.giphy.com/media/3gNowi8xp8PoWwdb0T/giphy.mp4'
-  },
-  {
-    trackNumber: 'C6',
-    title: 'Midi Break 2',
-    youtubeId: 'RaWxiCl8Dn8',
-    gif: 'https://media.giphy.com/media/Zy7HTSu2oD2f6VZEfl/giphy-downsized-large.gif',
-    video: 'https://media.giphy.com/media/Zy7HTSu2oD2f6VZEfl/giphy.mp4'
-  }],
-  [{
-    trackNumber: 'D1',
-    title: 'Shrimp',
-    youtubeId: 'RaWxiCl8Dn8',
-    gif: 'https://media.giphy.com/media/24FIhzP88RWJoO7Igt/giphy.gif',
-    video: 'https://media.giphy.com/media/24FIhzP88RWJoO7Igt/giphy.mp4'
-  },
-  {
-    trackNumber: 'D2',
-    title: 'All my Circuits',
-    youtubeId: 'RaWxiCl8Dn8',
-    gif: 'https://media.giphy.com/media/Zy7HTSu2oD2f6VZEfl/giphy-downsized-large.gif',
-    video: 'https://media.giphy.com/media/Zy7HTSu2oD2f6VZEfl/giphy.mp4'
-  },
-  {
-    trackNumber: 'D3',
-    title: 'Burning 4 U feat. Chrissley Benz',
-    youtubeId: 'RaWxiCl8Dn8',
-    gif: 'https://media.giphy.com/media/Zy7HTSu2oD2f6VZEfl/giphy-downsized-large.gif',
-    video: 'https://media.giphy.com/media/Zy7HTSu2oD2f6VZEfl/giphy.mp4'
-  },
-  {
-    trackNumber: 'D4',
-    title: 'Exit',
-    youtubeId: 'RaWxiCl8Dn8',
-    gif: 'https://media.giphy.com/media/Zy7HTSu2oD2f6VZEfl/giphy-downsized-large.gif',
-    video: 'https://media.giphy.com/media/Zy7HTSu2oD2f6VZEfl/giphy.mp4'
-  }]
+	[
+		{
+			trackNumber: 'A1',
+			title: 'Entrance',
+			youtubeId: 'TvW90E8UAIw',
+			giphyId: 'ncslB4NOOZE0DiAEX3'
+		},
+		{
+			trackNumber: 'A2',
+			title: 'Level Zero',
+			youtubeId: '',
+			giphyId: ''
+		},
+		{
+			trackNumber: 'A3',
+			title: 'No Hay Trabajo Para Mi',
+			youtubeId: 'QWLzoBwOsmM',
+			giphyId: 'FDcvH6Icd9WAQRxHHx'
+		},
+		{
+			trackNumber: 'A4',
+			title: 'Albania FM feat. Funkycan',
+			youtubeId: '89XVesYvpNs',
+			giphyId: '46iZk96KFDzKcQVNVw'
+			// 3gNowi8xp8PoWwdb0T
+		}
+	],
+	[
+		{
+			trackNumber: 'B1',
+			title: 'Intershop',
+			youtubeId: '2ULvWsd6ETU',
+			giphyId: '3gILNf4GtErGOZWMIL'
+		},
+		{
+			trackNumber: 'B2',
+			title: 'Midi Break 1',
+			youtubeId: '3dT4df8Eqi8',
+			giphyId: 'bwLlHwgZMbqJYoVFI1'
+		},
+		{
+			trackNumber: 'B3',
+			title: 'Srrr',
+			youtubeId: '',
+			giphyId: ''
+		},
+		{
+			trackNumber: 'B4',
+			title: 'Midi Lisa',
+			youtubeId: 'ST7RG2x8-Bw',
+			giphyId: 'EprhCzmAK8fhGtGipk'
+		},
+		{
+			trackNumber: 'B5',
+			title: 'Funk',
+			youtubeId: '',
+			giphyId: ''
+		},
+		{
+			trackNumber: 'B6',
+			title: 'Master Quest',
+			youtubeId: '9mtYpSeu_zQ',
+			giphyId: '9J1vk1p1mmgukgm4vb'
+		}
+	],
+	[
+		{
+			trackNumber: 'C1',
+			title: 'Y',
+			youtubeId: 'bA0yHM7sW7o',
+			giphyId: ''
+		},
+		{
+			trackNumber: 'C2',
+			title: 'Arcadia',
+			youtubeId: '',
+			giphyId: ''
+		},
+		{
+			trackNumber: 'C3',
+			title: 'Termina feat. Gerry Franke',
+			youtubeId: 'fVV8Am1r_94',
+			giphyId: '8JNgUQYmyBLFK9yUnl'
+			// giphyId: '3q3JCzblNJ3LP1h0xm'
+		},
+		{
+			trackNumber: 'C4',
+			title: 'Rush',
+			youtubeId: 'PkDW2ztY3yE',
+			giphyId: '5zvO54yAthO2C9g2ZJ'
+		},
+		{
+			trackNumber: 'C5',
+			title: 'Lozt feat. Ludi',
+			youtubeId: 'XD8fiRz05W0',
+			giphyId: 'B13I4WMx1T1UxEdLl5'
+		},
+		{
+			trackNumber: 'C6',
+			title: 'Midi Break 2',
+			youtubeId: 'Xd4ZLweqMx4',
+			giphyId: '57SRy3NTEYvAhwWxU7' // part1
+			// https://www.youtube.com/watch?v=j8fgDCuGnsU // part2
+			// https://www.youtube.com/watch?v=SrRfGl5ZyWk // part3
+			// https://www.youtube.com/watch?v=lDcBYgOP7Cg // part4
+		}
+	],
+	[
+		{
+			trackNumber: 'D1',
+			title: 'Shrimp',
+			youtubeId: '',
+			giphyId: ''
+		},
+		{
+			trackNumber: 'D2',
+			title: 'All my Circuits',
+			youtubeId: 'nd5hD3zgS-k',
+			giphyId: 'x4bUrkOjS9ZDQUVgH2'
+		},
+		{
+			trackNumber: 'D3',
+			title: 'Burning 4 U feat. Chrissley Benz',
+			youtubeId: 'FGz4R2xeGaE',
+			giphyId: '2YiSv8lvmBKgHDreya'
+		},
+		{
+			trackNumber: 'D4',
+			title: 'Exit',
+			youtubeId: 'E-b14ul9egg',
+			giphyId: '1SuFqrHCDlWvxAOL9P'
+		}
+	]
 ]
